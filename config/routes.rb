5a49cc3387b49ac4207f3b6ff448eee8f7c387ab
Kevin Austin <kevinaustin@hotmail.com>
2014-08-27 17:10:01 -0700

    Add some feature

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,11 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
+namespace :api do
+  namespace :v1 do
+    resources :doctors
+  end
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
